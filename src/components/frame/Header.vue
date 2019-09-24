e73f14dc3fd1c9ddd48d9363440c26fc31308526
monica835 <54056666+monica835@users.noreply.github.com>
2019-09-24 21:00:24 +0800

    Add files via upload

--- src/components/frame/Header.vue
+++ src/components/frame/Header.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#" id="title">FIRSTAPP</b-navbar-brand>
+      <b-navbar-brand href="#" id="title">Kat-On</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-navbar-nav class="ml-auto">
         <b-nav-item v-on:click="redirect('/Login')">Login</b-nav-item>
         <b-nav-item @click="redirect('/Register')" >Register</b-nav-item>
+        <b-nav-item @click="redirect('/Logout')" >Logout</b-nav-item>
       </b-navbar-nav>
     </b-navbar>
   </div>
